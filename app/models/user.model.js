7a152174439681368971336e2e6d15d85b27545c
Usenmfon <usenmfonuko@gmail.com>
2021-12-23 14:12:29 +0100

    updated user model

--- app/models/user.model.js
+++ app/models/user.model.js
@@ -4,6 +4,7 @@ const User = mongoose.model(
     "User",
     new mongoose.Schema({
         fullname: { type: String, required: [true, 'enter a fullname'] },
+        username: { type: String, required: [true, 'enter a fullname'] },
         email: { type: String, required: [true, 'enter an email '] },
         password: { type: String, required: [true, 'enter a password '] },
         referral: { type: String },
